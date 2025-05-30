3b2f137e1f8d00078508f7623a67b2c42b9be991
Heedong <donghui11777@gmail.com>
2025-05-30 16:43:49 +0900

    feat:BPT5-14: 초기 글로벌 스타일 생성

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Noto Sans KR', sans-serif,
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+    cursor: pointer;
+  }
+
+  ul, ol {
+    list-style: none; 
+  }
+`;
+
+export default GlobalStyle;
