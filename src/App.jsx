324fef3961466e74483497cdb62392b53c046c64
Heedong <donghui11777@gmail.com>
2025-05-30 17:40:13 +0900

    feat:BPT5-29:메인 페이지로 리다이렉트 루트 경로로 진입 시, 메인 페이지로 리다이렉트하는 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import Header from './layouts/Header';
 import CreateShopPage from './pages/CreateShopPage';
@@ -13,6 +13,8 @@ const App = () => {
       <GlobalStyle />
       <Routes>
         <Route path='/' element={<Header />}>
+          {/* 메인 페이지로 리다이렉트 */}
+          <Route index element={<Navigate to={'/list'} replace={true} />} />
           {/* 메인 페이지 */}
           <Route path='/list' element={<MainPage />}></Route>
           {/* 상세 페이지 */}
