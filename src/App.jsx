5fecc2eea759117e91b0d8f8a01e49fc1298621c
Heedong <donghui11777@gmail.com>
2025-05-30 17:25:52 +0900

    feat:BPT5-28: 리액트 라우팅 추가 간단한 라우팅 구조를 구현하여 기본적인 페이지 전환 구조를 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Header from './layouts/Header';
+import CreateShopPage from './pages/CreateShopPage';
+import DetailShopPage from './pages/DetailShopPage';
+import MainPage from './pages/MainPage';
+import UpdateShopPage from './pages/UpdateShopPage';
+import GlobalStyle from './styles/GlobalStyle';
+
 const App = () => {
-  return <h1>Initialize</h1>;
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes>
+        <Route path='/' element={<Header />}>
+          {/* 메인 페이지 */}
+          <Route path='/list' element={<MainPage />}></Route>
+          {/* 상세 페이지 */}
+          <Route path='/link/:URLid' element={<DetailShopPage />}></Route>
+        </Route>
+        {/* 생성 페이지 */}
+        <Route path='/linkpost' element={<CreateShopPage />}></Route>
+        {/* 수정 페이지 */}
+        <Route path='/link/:URLid/edit' element={<UpdateShopPage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
