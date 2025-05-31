8189e6e915a360a4f2c6c8b2f6d76996818e62bd
Heedong <donghui11777@gmail.com>
2025-05-31 16:03:56 +0900

    feat:BPT5-14: GlobalStyle 개선 초기화에 중점을 두고 이후 스타일을 적용하기 쉽도록 GlobalStyle을 수정했습니다.

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,5 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
 
+import { applyFontStyles } from './mixins';
+import { ColorTypes, FontTypes } from './theme';
+
 const GlobalStyle = createGlobalStyle`
   * {
     margin: 0;
@@ -12,7 +15,9 @@ const GlobalStyle = createGlobalStyle`
   }
 
   body {
-    font-family: 'Noto Sans KR', sans-serif;
+    font-family: Pretendard ,'Noto Sans KR', sans-serif;
+    ${applyFontStyles(FontTypes.REGULAR16, ColorTypes.SECONDARY_BLACK)}
+    line-height: 1.5;
   }
 
   a {
@@ -23,6 +28,26 @@ const GlobalStyle = createGlobalStyle`
 
   ul, ol {
     list-style: none; 
+  }
+
+  img {
+    display: block; /* 인라인 요소의 하단 여백 제거 */
+    max-width: 100%; /* 부모 너비에 맞춰 크기 조정 */
+    height: auto; /* 이미지 비율 유지 */
+  }
+
+  button, input, select, textarea {
+    font-family: inherit;
+    font-size: inherit; 
+    line-height: inherit;
+    margin: 0;
+    padding: 0;
+    border: none;
+    background: none;
+  }
+
+  button {
+    cursor: pointer;
   }
 `;
 
