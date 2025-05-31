fab35f51923f93e8b1fe8c06bd5578e5a1dbe06d
sinyroom <wldmsdl7913@naver.com>
2025-05-31 16:13:30 +0900

    feat: 생성/수정 페이지에 토스트 알림 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import Header from './layouts/Header';
 import CreateShopPage from './pages/CreateShopPage';
@@ -5,11 +6,15 @@ import DetailShopPage from './pages/DetailShopPage';
 import MainPage from './pages/MainPage';
 import UpdateShopPage from './pages/UpdateShopPage';
 import GlobalStyle from './styles/GlobalStyle';
+import Toast from './Toast';
 
 const App = () => {
+  const [toastMessage, setToastMessage] = useState('');
+
   return (
     <BrowserRouter>
       <GlobalStyle />
+      <Toast message={toastMessage} />
       <Routes>
         <Route path='/' element={<Header />}>
           {/* 메인 페이지로 리다이렉트 */}
@@ -20,9 +25,19 @@ const App = () => {
           <Route path='/link/:URLid' element={<DetailShopPage />}></Route>
         </Route>
         {/* 생성 페이지 */}
-        <Route path='/linkpost' element={<CreateShopPage />}></Route>
+        <Route
+          path='/linkpost'
+          element={
+            <CreateShopPage onSuccess={() => setToastMessage('생성 완료')} />
+          }
+        ></Route>
         {/* 수정 페이지 */}
-        <Route path='/link/:URLid/edit' element={<UpdateShopPage />}></Route>
+        <Route
+          path='/link/:URLid/edit'
+          element={
+            <UpdateShopPage onSuccess={() => setToastMessage('수정 완료')} />
+          }
+        ></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/Toast.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const StyledToastContainer = styled(ToastContainer)`
+  .Toastify__close-button {
+    display: none;
+  }
+
+  .Toastify__toast {
+    background-color: #6166e9 !important;
+    color: #ffffff !important;
+    width: 96px !important;
+    height: 46px !important;
+    min-height: 0 !important;
+    min-width: 0 !important;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    padding: 0 !important;
+  }
+`;
+
+function Toast({ message }) {
+  useEffect(() => {
+    if (message) {
+      toast(message);
+    }
+  }, [message]);
+
+  return (
+    <StyledToastContainer
+      position='bottom-center'
+      autoClose={3000}
+      hideProgressBar={true}
+      pauseOnHover
+      pauseOnFocusLoss
+      closeOnClick
+      theme='light'
+      limit={1}
+    />
+  );
+}
+
+export default Toast;
