288af01fa6977b7e8b405ac8dac003d5f8e9c4e0
Heedong <donghui11777@gmail.com>
2025-05-31 14:41:10 +0900

    feat:BPT5-14: theme.js 추가 전역 스타일을 테마로 추가

---
+++ src/styles/theme.js
@@ -0,0 +1,70 @@
+const theme = {
+  colors: {
+    pri: '#3e45ec',
+    err: '#fb545b',
+    secBlack: '#14151a',
+    secGray100: '#888790',
+    secGray200: '#a2a2ac',
+    secGray300: '#dddcdf',
+    secWhite100: '#79747e',
+    secWhite200: '#fafafb',
+    secWhite300: '#a4a1aa',
+  },
+
+  fonts: {
+    bold20: {
+      fontSize: '1.25rem',
+      fontWeight: 700,
+    },
+    medium18: {
+      fontSize: '1.125rem',
+      fontWeight: 500,
+    },
+    regular18: {
+      fontSize: '1.125rem',
+      fontWeight: 400,
+    },
+    semibold17: {
+      fontSize: '1.063rem',
+      fontWeight: 600,
+    },
+    medium17: {
+      fontSize: '1.063rem',
+      fontWeight: 500,
+    },
+    regular17: {
+      fontSize: '1.063rem',
+      fontWeight: 400,
+    },
+    bold16: {
+      fontSize: '1rem',
+      fontWeight: 700,
+    },
+    semibold16: {
+      fontSize: '1rem',
+      fontWeight: 600,
+    },
+    medium16: {
+      fontSize: '1rem',
+      fontWeight: 500,
+    },
+    regular16: {
+      fontSize: '1rem',
+      fontWeight: 400,
+    },
+    semibold15: {
+      fontSize: '0.938rem',
+      fontWeight: 600,
+    },
+    medium15: {
+      fontSize: '0.938rem',
+      fontWeight: 500,
+    },
+    semibold14: {
+      fontSize: '0.875rem',
+      fontWeight: 600,
+    },
+  },
+};
+
+export default theme;
