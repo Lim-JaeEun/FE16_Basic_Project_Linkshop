e714d15b13c7ca5efc71782091a7533bcd61e762
임재은 <jaeeunlim@imjaeeun-ui-MacBookPro.local>
2025-06-04 18:43:15 +0900

    크로스브라우징 테스트2

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -21,6 +21,16 @@ const ProductImgWrapper = styled.div`
   gap: 12px;
   flex-wrap: nowrap;
   overflow-x: scroll;
+  /* WebKit 기반 브라우저 (Chrome, Safari, Edge) 스크롤바 숨기기 */
+  &::-webkit-scrollbar {
+    display: none; /* 스크롤바 트랙 자체를 숨김 */
+    width: 0; /* 폭을 0으로 설정하여 공간 차지하지 않게 함 */
+    height: 0; /* 높이를 0으로 설정하여 공간 차지하지 않게 함 */
+  }
+
+  /* Firefox 스크롤바 숨기기 (또는 얇게 만들기) */
+  scrollbar-width: none; /* 'none' 또는 'thin' */
+  -ms-overflow-style: none; /* IE 및 Edge (레거시) 스크롤바 숨기기 */
 `;
 const TotalProducts = styled.div`
   ${({ $fontType = FontTypes.REGULAR16 }) => applyFontStyles($fontType)};
