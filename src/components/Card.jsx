17a44598dd392ed92839fc8b46fd183729713c9b
임재은 <jaeeunlim@imjaeeun-ui-MacBookPro.local>
2025-06-09 15:39:04 +0900

    캐러셀-커스텀훅 분리완료

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,5 @@
-import { useRef, useEffect, useState } from 'react';
+import { useState } from 'react';
 
-import throttle from 'lodash.throttle';
 import Slider from 'react-slick';
 import styled, { keyframes } from 'styled-components';
 
@@ -10,6 +9,7 @@ import LinkshopProfileInfo from './LinkshopProfileInfo';
 import btn_back from '../assets/icon/btn_back.png';
 import { useCardData } from '../hooks/useCardsData';
 import { useIsLargeScreen } from '../hooks/useIsLargeScreen';
+import { useSlider } from '../hooks/useProductSlider';
 import { applyFontStyles } from '../styles/mixins';
 import { FontTypes } from '../styles/theme';
 
@@ -46,7 +46,8 @@ const CardWrapper = styled.div`
     &:hover {
       animation: ${fadeInShadow} 0.6s forwards;
       cursor: pointer;
-    }  
+    }
+  }
 `;
 
 const ProductImgWrapper = styled.div`
@@ -54,7 +55,6 @@ const ProductImgWrapper = styled.div`
 `;
 
 const CustomSliderWrapper = styled(Slider)`
-
   .slick-prev,
   .slick-next {
     position: absolute;
@@ -83,7 +83,6 @@ const CustomSliderWrapper = styled(Slider)`
     }
   }
 
-  
   .slick-disabled {
     opacity:0;
   }
@@ -114,7 +113,6 @@ const TotalProducts = styled.div`
   ${({ $fontType = FontTypes.REGULAR16 }) => applyFontStyles($fontType)};
 `;
 function Card({ cardData }) {
-  const [showAmount, setShowAmount] = useState(1);
   const isLargeScreen = useIsLargeScreen(768);
   const [isTouched, setIsTouched] = useState(false); //모바일, 태블릿모드 감지
   const {
@@ -130,95 +128,20 @@ function Card({ cardData }) {
     },
     handleToggleLike,
   } = useCardData(cardData);
+  const {
+    sliderRef,
+    wrapperRef,
+    sliderSettings,
+    handleAutoPlay,
+    handleRemoveAutoPlay,
+  } = useSlider(isLargeScreen);
 
-  const sliderRef = useRef(null);
-  const wrapperRef = useRef(null);
-
-  const sliderSettings = {
-    dots: false, // 하단에 점(dot) 내비게이션 표시
-    infinite: false, // 마지막 슬라이드에서 멈춤 (false: 루프 안 함, true: 루프 함)
-    speed: 500, // 슬라이드 전환 속도
-    slidesToShow: showAmount, // 한 번에 보여줄 슬라이드 개수
-    slidesToScroll: 1, // 한 번 스크롤/슬라이드 시 넘어갈 슬라이드 개수
-    arrows: false, // 좌우 화살표 내비게이션 표시
-    draggable: true,
-    cssEase: 'linear',
-    autoplay: false, // 초기 자동 재생은 false로 설정합니다.
-    autoplaySpeed: 1000,
-    pauseOnHover: true,
-    responsive: [
-      {
-        breakpoint: 1024, // 화면 너비가 1024px 이하일 때, (아이패드프로기준)
-        settings: {
-          arrows: true,
-        },
-      },
-    ],
-  };
-
-  const handleAutoPlay = () => {
-    if (sliderRef.current) {
-      sliderRef.current.slickPlay(); // slickPlay() 메서드 호출
-    }
-  };
-
-  // 마우스 이탈 시 자동 재생 정지
-  const handleRemoveAutoPlay = () => {
-    if (sliderRef.current) {
-      sliderRef.current.slickPause(); // slickPause() 메서드 호출
-    }
-  };
-
-  const calcHowManySlides = (layoutWidth, itemWidth, gap) => {
-    if (
-      layoutWidth === null ||
-      layoutWidth === undefined ||
-      layoutWidth <= 0 ||
-      isNaN(layoutWidth)
-    ) {
-      return 1; // 유효하지 않은 값일 경우 최소 1개 반환
-    }
-    if (itemWidth + gap <= 0) {
-      return 1; // 0으로 나누기 방지
-    }
-    const calculatedSlides = Math.floor(
-      (layoutWidth + gap) / (itemWidth + gap),
-    );
-
-    return Math.max(1, calculatedSlides); // 최소 1개 보장
-  };
   const handlerTouchStart = () => {
     setIsTouched(true);
   };
   const handlerTouchEnd = () => {
     setIsTouched(false);
   };
-  useEffect(() => {
-    if (sliderRef.current) {
-      sliderRef.current.slickGoTo(0, true);
-    }
-  }, [isLargeScreen]);
-
-  useEffect(() => {
-    const updateListWidthAndShowAmount = () => {
-      if (wrapperRef.current) {
-        setShowAmount(calcHowManySlides(wrapperRef.current.offsetWidth, 95, 8));
-      }
-    };
-
-    const throttleEvent = throttle(updateListWidthAndShowAmount, 200, {
-      leading: true,
-      trailing: true,
-    });
-
-    updateListWidthAndShowAmount();
-
-    window.addEventListener('resize', throttleEvent);
-
-    return () => {
-      window.removeEventListener('resize', throttleEvent);
-    };
-  }, []);
 
   return (
     <CardWrapper
